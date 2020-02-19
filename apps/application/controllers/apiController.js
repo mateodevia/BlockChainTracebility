ff1632f0c04770e6879f6cbe526bb76b9fdc77c0
mateo <m.devia@uniandes.edu.co>
2020-02-19 16:57:26 -0500

    async fixed

--- apps/application/controllers/apiController.js
+++ apps/application/controllers/apiController.js
@@ -3,10 +3,13 @@
 const getGateway = require('../gateway/gateway');
 
 module.exports.get = (req, res) => {
-  const contract = network.getContract('fabcar');
-  const result = await contract.submitTransaction('metodoPrueba', 'a', 'b', 'c');
-  res.status(200);
-  res.status(200).json({ msg: 'Esto es una prueba' });
+  return getGateway.then(async ({ gateway, network }) => {
+    const contract = network.getContract('fabcar');
+    const result = await contract.submitTransaction('metodoPrueba', 'a', 'b', 'c');
+    // let response = JSON.parse(result.toString());
+
+    res.status(200).json({ msg: 'Esto es una prueba' });
+  });
 };
 
 module.exports.post = (req, res) => {
