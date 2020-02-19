071a2e1b3038a63f71ae0d976919f25f3a51a9ab
mateo <m.devia@uniandes.edu.co>
2020-02-19 16:13:00 -0500

    first change

--- apps/application/app.js
+++ apps/application/app.js
@@ -26,17 +26,17 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
   res.sendFile(__dirname + '/public/index.html');
 });
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- apps/application/controllers/apiController.js
+++ apps/application/controllers/apiController.js
@@ -4,16 +4,16 @@ const getGateway = require('../gateway/gateway');
 
 module.exports.get = (req, res) => {
   res.status(200);
-  res.status(200).json({msg: 'hi'});
+  res.status(200).json({ msg: 'El blockchain del cachorro' });
 };
 
 module.exports.post = (req, res) => {
-  return getGateway.then(async ({gateway, network})=>{
+  return getGateway.then(async ({ gateway, network }) => {
     const contract = network.getContract('fabcar');
     const result = await contract.submitTransaction('invoke', '1', '2', '3');
 
     // let response = JSON.parse(result.toString());
 
-    res.status(200).json({invoked: 'ok'});
+    res.status(200).json({ invoked: 'ok' });
   });
 };
