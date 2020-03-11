d08fc7bb0ddecbebe0385188da6d1c3a8831cfc7
mateo <m.devia@uniandes.edu.co>
2020-03-10 20:51:51 -0500

    try

--- apps/application/controllers/apiController.js
+++ apps/application/controllers/apiController.js
@@ -5,6 +5,7 @@ const getGateway = require('../gateway/gateway');
 module.exports.metodoPrueba = (req, res) => {
   return getGateway.then(async ({ gateway, network }) => {
     const contract = network.getContract('fabcar');
+    console.log('llego al endpoint de metodoPrueba');
     const result = await contract.submitTransaction('metodoPrueba', 'a', 'b', 'c');
     res.status(200).json(result);
   });
@@ -13,6 +14,7 @@ module.exports.metodoPrueba = (req, res) => {
 module.exports.get = (req, res) => {
   return getGateway.then(async ({ gateway, network }) => {
     const contract = network.getContract('fabcar');
+    console.log('llego al endpoint de get');
     const result = await contract.submitTransaction('invoke', '1', '3', '1');
     // let response = JSON.parse(result.toString());
     res.status(200).json(result);
