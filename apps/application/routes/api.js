29d148a11842638678d9dbad07a1d63265991b70
mateo <m.devia@uniandes.edu.co>
2020-03-11 09:30:45 -0500

    all to get

--- apps/application/routes/api.js
+++ apps/application/routes/api.js
@@ -7,6 +7,6 @@ const proveedor = require('../controllers/proveedorController');
 
 router.get('/', apiController.get);
 router.get('/metodoPrueba', apiController.metodoPrueba);
-router.post('/proveedor', proveedor.registrarProveedor);
+router.get('/proveedor', proveedor.registrarProveedor);
 
 module.exports = router;
--- apps/application/routes/index.js
+++ apps/application/routes/index.js
@@ -15,12 +15,6 @@ const fs = require('fs');
 const swaggerDocument = yaml.safeLoad(fs.readFileSync('./swagger.yaml'));
 
 router.use('/v1/api', apiRouter);
-router.use('/consumidor', consumidor);
-router.use('/proveedor', proveedor);
-router.use('/transportador', transportador);
-router.use('/transformador', transformador);
-router.use('/distribuidor', distribuidor);
-router.use('/enteRegulador', enteRegulador);
 router.use('/api-docs', swaggerUi.serve);
 router.get('/api-docs', swaggerUi.setup(swaggerDocument));
 
