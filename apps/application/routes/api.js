7b7e94f0a42b8735982ad6b2681189f742f7b36b
mateo <m.devia@uniandes.edu.co>
2020-03-11 09:26:10 -0500

    new routing strategy

--- apps/application/routes/api.js
+++ apps/application/routes/api.js
@@ -3,8 +3,10 @@
 const express = require('express');
 let router = express.Router();
 const apiController = require('../controllers/apiController');
+const proveedor = require('../controllers/proveedorController');
 
 router.get('/', apiController.get);
 router.get('/metodoPrueba', apiController.metodoPrueba);
+router.post('/proveedor', proveedor.registrarProveedor);
 
 module.exports = router;
