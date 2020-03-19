f9d1cdac3fb2d336e35c7fe0b76b27948faec460
m.devia@uniandes.edu.co <m.devia@uniandes.edu.co>
2020-03-19 18:21:24 -0500

    getTruById ready with http response

--- apps/application/controllers/apiController.js
+++ apps/application/controllers/apiController.js
@@ -18,7 +18,6 @@ module.exports.getTruById = (req, res) => {
   return getGateway.then(async ({ gateway, network }) => {
     const contract = network.getContract('fabcar');
     let response = await contract.evaluateTransaction('getTruById', req.body.id.toString());
-    console.log(response.toString());
-    res.status(200).json({ msg: 'Actor registrado correctamente' });
+    res.status(200).json(response.toString());
   });
 };
