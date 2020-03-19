ebd3b4a7705be2d16476757668f00cc759f115ef
m.devia@uniandes.edu.co <m.devia@uniandes.edu.co>
2020-03-19 18:00:29 -0500

    get tru by id draft 1

--- apps/application/routes/api.js
+++ apps/application/routes/api.js
@@ -6,6 +6,7 @@ const apiController = require('../controllers/apiController');
 const consumidor = require('../controllers/consumidorController');
 const proveedor = require('../controllers/proveedorController');
 
+router.get('/tru/', apiController.getTruById);
 router.post('/actor', apiController.registrarActor);
 router.post('/consumidor/consumir', consumidor.consumir);
 
