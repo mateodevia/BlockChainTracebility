903c3bf1537d7e888afd77773e4a1bf8e1116e0e
m.devia@uniandes.edu.co <m.devia@uniandes.edu.co>
2020-03-19 17:04:52 -0500

    digital certificate added to db

--- apps/application/controllers/consumidorController.js
+++ apps/application/controllers/consumidorController.js
@@ -4,15 +4,14 @@ const getGateway = require('../gateway/gateway');
 const fabric_network = require('fabric-network');
 
 module.exports.consumir = (req, res) => {
-  let id = '12345';
-  let trus_consumidos = req.body.trus;
-  let ubicacion = req.body.ubicacion;
-  let actor = '123456';
   return getGateway.then(async ({ gateway, network }) => {
+    let id = '123456';
+    let trus_consumidos = req.body.trus;
+    let ubicacion = req.body.ubicacion;
+    let actor = gateway.client._userContext._identity._certificate;
     const contract = network.getContract('fabcar');
     let args = JSON.stringify([id, actor, ubicacion, trus_consumidos]);
     await contract.submitTransaction('consumir', args);
-    console.log('ACAAAA', gateway.client._userContext._identity._certificate);
     res.status(200).json({ msg: 'Trus consumidos correctamente' });
   });
 };
