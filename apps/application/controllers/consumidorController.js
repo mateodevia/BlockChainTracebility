872cbe74276d7809ccb338ad2d676bf58a905b0f
m.devia@uniandes.edu.co <m.devia@uniandes.edu.co>
2020-03-19 17:19:24 -0500

    uuid added

--- apps/application/controllers/consumidorController.js
+++ apps/application/controllers/consumidorController.js
@@ -2,10 +2,11 @@
 
 const getGateway = require('../gateway/gateway');
 const fabric_network = require('fabric-network');
+const { v1 } = require('uuid');
 
 module.exports.consumir = (req, res) => {
   return getGateway.then(async ({ gateway, network }) => {
-    let id = '123456';
+    let id = v1();
     let trus_consumidos = req.body.trus;
     let ubicacion = req.body.ubicacion;
     let actor = gateway.client._userContext._identity._certificate;
