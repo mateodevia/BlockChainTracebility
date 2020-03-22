7376eaa16a4a72f4497e0677b2abe904ab020a0d
m.devia@uniandes.edu.co <m.devia@uniandes.edu.co>
2020-03-22 11:36:35 -0500

    invalidar route added

--- apps/application/routes/api.js
+++ apps/application/routes/api.js
@@ -7,6 +7,7 @@ const apiController = require('../controllers/apiController');
 const consumidor = require('../controllers/consumidorController');
 const proveedor = require('../controllers/proveedorController');
 const transportador = require('../controllers/transportadorController');
+const regulador = require('../controllers/reguladorController');
 
 router.get('/tru/id/:id', apiController.getTruById);
 router.get('/tru/sku/:sku', apiController.getTruBySku);
@@ -15,5 +16,6 @@ router.post('/transaccion', apiController.crearTransaccion);
 router.post('/actividad/consumir', consumidor.consumir);
 router.post('/actividad/producir', proveedor.producir);
 router.post('/actividad/transportar', transportador.transportar);
+router.post('/actividad/invalidar', regulador.invalidar);
 
 module.exports = router;
