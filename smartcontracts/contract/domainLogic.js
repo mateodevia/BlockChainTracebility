055bbde7b7faf797fbdd3ac731e3f73c78e61193
m.devia@uniandes.edu.co <m.devia@uniandes.edu.co>
2020-03-22 17:40:18 -0500

    business logic added

--- smartcontracts/contract/domainLogic.js
+++ smartcontracts/contract/domainLogic.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-throw-literal */
 /* eslint-disable linebreak-style */
 'use strict';
 
@@ -10,13 +11,22 @@ module.exports.transformar = (trus_consumidos, trus_producidos) => {
   console.log('##### Verificando que la transformacion cumpla las reglas #####');
   let suma_consumidos = 0;
   for (let i in trus_consumidos) {
-    let cantidad = parseInt(trus_consumidos[i][1].cantidad.replace('kg', ''));
+    let cantidad = parseFloat(trus_consumidos[i][1].cantidad.replace('kg', ''));
     suma_consumidos += cantidad;
   }
 
   let suma_producidos = 0;
   for (let i in trus_producidos) {
-    let cantidad = parseInt(trus_producidos[i].cantidad.replace('kg', ''));
+    let cantidad = parseFloat(trus_producidos[i].cantidad.replace('kg', ''));
     suma_producidos += cantidad;
+  }
+
+  let rendimiento = suma_producidos * 100 / suma_consumidos;
+
+  if (rendimiento < 99) {
+    throw 'No puede haber tanto desperdicio en una transformacion de plastico';
+  }
+  if (rendimiento > 100) {
+    throw 'Los TRUs consumidos no son suficientes para producir los TRUs producidos';
   }
 };
