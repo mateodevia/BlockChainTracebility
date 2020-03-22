f7f45d68a47ecad8714c65d1b8fa21046f39c53b
m.devia@uniandes.edu.co <m.devia@uniandes.edu.co>
2020-03-22 17:19:48 -0500

    transformation business logic first draft

--- smartcontracts/contract/domainLogic.js
+++ smartcontracts/contract/domainLogic.js
@@ -3,11 +3,23 @@
 
 module.exports.invalidar = (tru) => {
   console.log('##### Verificando si se puede invalidar #####');
-  return true;
 };
 
 module.exports.transformar = (trus_consumidos, trus_producidos) => {
-  //trus_consumidos es un arreglo de tuplas [is, tru]
+  //trus_consumidos es un arreglo de tuplas [id, tru]
   console.log('##### Verificando que la transformacion cumpla las reglas #####');
-  return true;
+  let suma_consumidos = 0;
+  for (let i in trus_consumidos) {
+    let cantidad = parseInt(trus_consumidos[i][1].cantidad.replace('kg', ''));
+    suma_consumidos += cantidad;
+  }
+
+  let suma_producidos = 0;
+  for (let i in trus_producidos) {
+    let cantidad = parseInt(trus_consumidos[i].cantidad.replace('kg', ''));
+    suma_producidos += cantidad;
+  }
+  console.log('consumidos', suma_consumidos);
+  console.log('producidos', suma_producidos);
+
 };
