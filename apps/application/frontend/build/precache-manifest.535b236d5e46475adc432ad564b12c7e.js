bcb27b4a7c7f6a1e39909eb3692adf92b48bd28b
mateodevia <m.devia@uniandes.edu.co>
2020-04-08 21:22:31 -0500

    frontend build removed from gitignore

---
+++ apps/application/frontend/build/precache-manifest.535b236d5e46475adc432ad564b12c7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfaed334b1960fd9b70544a6fd7f536a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cac4592467446a26aed8",
+    "url": "/static/css/main.f9bfdbb0.chunk.css"
+  },
+  {
+    "revision": "180e3b128c22821acd61",
+    "url": "/static/js/2.22f2546a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.22f2546a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cac4592467446a26aed8",
+    "url": "/static/js/main.138a7cf9.chunk.js"
+  },
+  {
+    "revision": "700d31d1c6f37b7bdc51",
+    "url": "/static/js/runtime-main.84578d5e.js"
+  }
+]);
